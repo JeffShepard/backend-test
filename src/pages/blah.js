d072bc59b9e0ceb5fc716dd1e0bd6a21ca94dfe0
Jeffrey Shepard <63379511+JeffShepard@users.noreply.github.com>
2022-02-11 14:41:54 -0600

    Update blah.js

--- src/pages/blah.js
+++ src/pages/blah.js
@@ -7,7 +7,6 @@ const Blah = () => {
     <div>
     <p> This is a page that says Blah</p>
     <h1> by Blah I really mean Blah </h1>
-    <a href={"https://jeffshepard.dev"}>Portfolio for you</a>
     </div>
   )
 }
