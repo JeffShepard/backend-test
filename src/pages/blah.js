b19f9376da330c6e669f6da906b7a3969276812d
Jeffrey Shepard <63379511+JeffShepard@users.noreply.github.com>
2022-02-11 14:19:57 -0600

    Create blah.js

---
+++ src/pages/blah.js
@@ -0,0 +1,12 @@
+import React  from 'react'
+
+const Blah = () => {
+//   const [counter, setCounter] = useState(0);
+  
+  return (
+    <div>
+    <p> This is a page that says Blah<p>
+    <h1> by Blah I really mean Blah <h1>
+    <br></br>
+    <a href={"https://jeffshepard.dev"}>Portfolio for you</a>
+    </div>
